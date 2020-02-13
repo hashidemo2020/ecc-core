e40b0ae1ec52e98fb74d0a6833d0ff0eff017da1
Prakash Manglanathan <pmnathan@gmail.com>
2020-02-13 09:28:11 -0500

    test2

--- 2-subnets/2-subnets.tf
+++ 2-subnets/2-subnets.tf
@@ -1,8 +1,6 @@
 
-
-
 resource "aws_subnet" "services" {
-  vpc_id     = data.vpc_remote_workspace.vpc.outputs.vpc_id
+  vpc_id     = data.terraform_remote_state.vpc.outputs.vpc_id
   cidr_block = "10.0.0.0/24"
   tags = var.default_tags
 }
