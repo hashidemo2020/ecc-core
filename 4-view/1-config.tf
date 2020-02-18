65ff24bf8bcdfa4263cfbaf234d1c373e0b15642
manjeet-github <49072542+manjeet-github@users.noreply.github.com>
2020-02-18 14:27:19 -0500

    Update 1-config.tf

--- 4-view/1-config.tf
+++ 4-view/1-config.tf
@@ -1,15 +1,4 @@
-data "terraform_remote_state" "subnet" {
-  backend = "remote"
-  
-  config = {
-    hostname = "app.terraform.io"
-    organization = var.remote_organization
-    token = var.token
-    workspaces = {
-      name = var.subnet_remote_workspace_name
-    }
-  }
-}
+
 
 resource "random_id" "random" {
   keepers = {
@@ -21,5 +10,18 @@ resource "random_id" "random" {
 resource "null_resource" "rotate" {
   triggers = {
     uuid_hex = random_id.random.hex
+  }
+}
+
+data "terraform_remote_state" "subnet" {
+  backend = "remote"
+  
+  config = {
+    hostname = "app.terraform.io"
+    organization = var.remote_organization
+    token = var.token
+    workspaces = {
+      name = var.subnet_remote_workspace_name
+    }
   }
 }
